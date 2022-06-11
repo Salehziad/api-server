62aa569894b094f3eacd9032f9c9d77be43e6af9
Salehziad <salehziad1999@gmail.com>
2022-06-11 20:45:19 +0300

    add handlers

--- src/server.js
+++ src/server.js
@@ -5,12 +5,16 @@ const logger=require('./middleWar/logger')
 // require routes
 const foodRouter=require('./routes/food');
 const clothesRouter=require('./routes/clothes');
+const handleNotFound=require('./handlers/404');
+const handleServerError=require('./handlers/500');
 // to read what inside body
 app.use(express.json());
 // use routes wich as glopal consider as middle war functions
 app.use(logger);
 app.use(foodRouter);
 app.use(clothesRouter);
+app.use('*',handleNotFound);
+app.use(handleServerError);
 // this used for console all methods and endpoints that used in the server
 const listEndpoints = require("express-list-endpoints"); // npm i express-list-endpoints
 console.log(listEndpoints(app));
